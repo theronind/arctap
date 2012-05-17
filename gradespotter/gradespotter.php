d1d278ae029f3941ec5baed5efcbe777c7db74c2
Christopher Kennedy <kennedysgarage@gmail.com>
2012-05-16 20:33:26 -0400

    converting over to php

---
+++ gradespotter/gradespotter.php
@@ -0,0 +1,31 @@
+<?php  $title = 'Grade Spotter |'; include '../header.php'; ?>
+
+<nav>
+<div class="container">
+
+	<ul class="prime-nav">
+		<li><a href="">About</a></li>
+		<li><a href="">Projects</a></li>
+	</ul>
+	
+	<a href="/" id="logo"><img src="/img/logo.png"/></a>
+	
+	<ul class="second-nav">
+		<li><a href="">Contact</a></li>
+		<li><a href="">Twitter</a></li>
+	</ul>
+
+</div><!--/container-->
+</nav>
+
+
+<footer>
+<div class="container">
+	<p id="copy">&copy; 2012 ArcTap | <a href="http://nytm.org/made/">Made in NYC</a></p>
+        <ul>
+			<li>Follow us on Twitter: <a href="http://twitter.com/ArcTap">@ArcTap</a></li>
+		</ul>
+</div><!--/container-->
+</footer>
+
+<?php include("../footer.php"); ?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php  $title = ''; include 'header.php'; ?>
+
+<div id="intro">
+<div class="container">
+	<h1>ArcTap</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex</p>
+	<img src="img/logo-fade2.png"/>
+</div><!--/container-->
+</div><!--/intro-->
+
+<div id="apps">
+	<h2><span>Our Apps</span></h2>
+	<div class="container">
+			<ul class="clearfix">
+				<li>
+					<a href="/gradespotter"><div><img src="img/gradespotter.png"/></div></a>
+					<h3>Grade Spotter</h3>
+					<!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p> -->
+					<p>A full-featured restaurant inspection guide for your iPhone.</p>
+				</li>
+				<li>
+					<a href=""><div><img src="img/cochlear.png"/></div></a>
+					<h3>Cochlear <small>(concept)</small></h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+				</li>
+				<li><span></span></li>
+			</ul>
+	</div><!--/container-->
+</div><!--/apps-->
+
+<?php include("footer.php"); ?>
