6604ac593c359a7f7baf9c7b9f450ff7051be92d
Christopher Kennedy <kennedysgarage@gmail.com>
2012-05-25 17:11:45 -0400

    updating the header, adding fonts

--- gradespotter/gradespotter.php
+++ gradespotter/gradespotter.php
@@ -1,22 +1,5 @@
 <?php  $title = 'Grade Spotter |'; $css = '/gradespotter/app.css'; include '../header.php'; ?>
 <link rel="stylesheet" href="/css/style.css">
-<nav>
-<div class="container">
-
-	<ul class="prime-nav">
-		<li><a href="">Work</a></li>
-		<li><a href="">About</a></li>
-	</ul>
-	
-	<a href="/" id="logo"><img src="/img/logo.png"/></a>
-	
-	<ul class="second-nav">
-		<li><a href="">Contact</a></li>
-		<li><a href="">Twitter</a></li>
-	</ul>
-
-</div><!--/container-->
-</nav>
 
 <div class="container">
 	<p>Hello</p>
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <title><?php echo $title ?> ArcTap</title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width">
+<link href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="/css/reset.css">
 <link rel="stylesheet" href="/css/style.css">
 <link rel="stylesheet" href="<?php echo $css ?>">
@@ -16,3 +17,16 @@
 </head>
 <body>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+<header>
+<div class="container">
+	<h1><a href="/"><img src="/img/logo.png"> ArcTap</a></h1>	
+	<nav>
+		<ul>
+			<li><a href="">About</a></li>
+			<li><a href="">Work</a></li>
+			<li><a href="">Contact</a></li>
+		</ul>
+	</nav>
+</div><!--/container-->
+</header>
