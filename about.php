d69d88f7ba2b865b1e125f37287429af8fa5dea8
Christopher Kennedy <kennedysgarage@gmail.com>
2012-05-27 14:14:06 -0400

    laying out the basic for the pages

--- about.php
+++ about.php
@@ -1,4 +1,34 @@
 <?php  $title = 'About'; include 'header.php'; ?>
 
+<div class="container">
+	<div class="g7">
+		<h1>About</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div><!--/g7-->
+	<div class="g5">
+		<img src="http://placehold.it/380x200"/>
+	</div><!--/g5-->
+	<div class="clear"></div>
+	<hr/>
+	<ul id="team">
+		<li class="g4">
+			<img src="http://placehold.it/300x200"/>
+			<h3>Christopher Kennedy<br/><span>Founder</span></h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+		</li>
+		<li class="g4">
+			<img src="http://placehold.it/300x200"/>
+			<h3>Aaron Hall<br/><span>Designer</span></h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+		</li>
+		<li class="g4">
+			<img src="http://placehold.it/300x200"/>
+			<h3>Richard Owens<br/><span>iOS Developer</span></h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+		</li>
+	</ul>
+
+	
+</div><!--/container-->
 
 <?php include("footer.php"); ?>
--- work/work.php
+++ work/work.php
@@ -2,7 +2,31 @@
 <link rel="stylesheet" href="/css/style.css">
 
 <div class="container">
-	<p>Hello</p>
+
+	<div class="g3">
+		<h2>Cochlear Remote</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</div><!--/g3-->
+	<div class="g9">
+		<img src="http://placehold.it/700x300"/>
+	</div><!--/g9-->
+	
+	<div class="g3">
+		<h2>Grade Spotter</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</div><!--/g3-->
+	<div class="g9">
+		<img src="http://placehold.it/700x300"/>
+	</div><!--/g9-->
+	
+	<div class="g3">
+		<h2>App Name</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</div><!--/g3-->
+	<div class="g9">
+		<img src="http://placehold.it/700x300"/>
+	</div><!--/g9-->
+	
 </div><!--/container-->
 
 <?php include("../footer.php"); ?>
