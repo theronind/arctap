4562116723ed0610fb162cb4f4a6819fcc46679e
Christopher Kennedy <kennedysgarage@gmail.com>
2012-05-27 14:16:07 -0400

    adding links

--- work/work.php
+++ work/work.php
@@ -6,6 +6,7 @@
 	<div class="g3">
 		<h2>Cochlear Remote</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		<p><a href="/work/cochlear/">Learn More &raquo;</a></p>
 	</div><!--/g3-->
 	<div class="g9">
 		<img src="http://placehold.it/700x300"/>
@@ -14,6 +15,7 @@
 	<div class="g3">
 		<h2>Grade Spotter</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		<p><a href="/work/gradespotter/">Learn More &raquo;</a></p>
 	</div><!--/g3-->
 	<div class="g9">
 		<img src="http://placehold.it/700x300"/>
@@ -22,6 +24,7 @@
 	<div class="g3">
 		<h2>App Name</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		<p><a href="/work/">Learn More &raquo;</a></p>
 	</div><!--/g3-->
 	<div class="g9">
 		<img src="http://placehold.it/700x300"/>
