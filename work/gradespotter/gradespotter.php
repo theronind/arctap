2e123e5051180400557c5f2a443932e14ba7928f
Christopher Kennedy <kennedysgarage@gmail.com>
2012-05-29 23:52:29 -0400

    added some filler

--- work/gradespotter/gradespotter.php
+++ work/gradespotter/gradespotter.php
@@ -12,8 +12,18 @@
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex</p>
 	</div>
 -->
-<img src="/work/gradespotter/img/intro.png"/>
+	<img src="/work/gradespotter/img/intro.png"/>
 </div><!--/container-->
 </section><!--/intro-->
+
+<div class="container">
+	<h1>The Concept</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	
+	<h1>Strategy &amp; Approach</h1>
+	
+	<h1>The App</h1>
+</div><!--/container-->
+
 
 <?php include("../../footer.php"); ?>
