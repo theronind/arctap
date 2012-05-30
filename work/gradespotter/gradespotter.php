41c819b887c8738003c702f46a316805919c769e
Christopher Kennedy <kennedysgarage@gmail.com>
2012-05-29 23:46:57 -0400

    trying out another image

--- work/gradespotter/gradespotter.php
+++ work/gradespotter/gradespotter.php
@@ -12,6 +12,7 @@
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex</p>
 	</div>
 -->
+<img src="/work/gradespotter/img/intro.png"/>
 </div><!--/container-->
 </section><!--/intro-->
 
