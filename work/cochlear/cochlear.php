29d6136a2284c596f36e6415ffae8b5bc8e87d70
Aaron Hall <me@aaron.im>
2012-06-07 14:39:19 -0400

    Issues fixed
    • fixed nav issue after resizing the browser
    • added stuffs to cochlear page

--- work/cochlear/cochlear.php
+++ work/cochlear/cochlear.php
@@ -1,10 +1,40 @@
 <?php  $title = 'Cochlear'; $css = '/work/cochlear/style.css'; include '../../header.php'; ?>
+
 <link rel="stylesheet" href="/css/style.css">
 
-<div class="container">
+<div id="intro_cochlear">
+	
+	<div class="container">
 
-	this is a test
+		<div class="g12">
+			
+			<h1>The new Nucleus&reg; 5 Remote Assistant</h1>
+			<h2>ArcTap wants to help Cochlear continue being the leader of global hearing solutions with a simple solution.</h2>
+			img
+			
+		</div>
 
-</div><!--/container-->
+	</div><!--/container-->
+	
+</div><!-- /#intro_cochlear -->
+
+<div id="the_concept">
+	
+	<div class="container">
+
+		<div class="g12">
+			
+			<div class="intro-header">
+				
+				<h2>The Concept</h2>
+				<p>Meet Aaron, a normal guy in his twenties, married to an awesome cochlear implant audiologist, with a kid on the way. He does most of his hobby work on his iPhone, and he also happens to be deaf. Currently Aaron is equipped with Cochlear Nucleus 5 and has to carry around a bulky remote to adjust the settings. Aaron never understood why the remote could not just be on his iPhone, so he decided to mock one up.</p>
+				
+			</div><!-- /.intro-header -->
+			
+		</div>
+
+	</div><!--/container-->
+	
+</div><!-- /#the_concept -->
 
 <?php include("../../footer.php"); ?>
