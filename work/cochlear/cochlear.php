3ea2a79855a51681c3e4f5e37ce582dc89b45d5c
Aaron Hall <me@aaron.im>
2012-06-11 22:15:19 -0400

    More changes/fixes to Cochlear

--- work/cochlear/cochlear.php
+++ work/cochlear/cochlear.php
@@ -70,4 +70,119 @@
 		
 	</div><!-- /#strategy_approach -->
 
+	<div id="final_designs">
+		
+		<div class="container">
+
+			<div class="g12">
+				
+				<div class="intro-header">
+					
+					<h2>Final Designs</h2>
+	
+				</div><!-- /.intro-header -->
+				
+			</div><!-- /.g12 -->
+			
+	        <div class="g6">
+
+				<figure class="row-hero">
+					<img src="../../img/work/cochlear/screenshot-adjustments.png" />
+					<figcaption class="caption">
+						<h3>Adjustments</h3>
+						<p>Easy to change volume or sensitivity instantly at anytime such as during meetings.</p>
+					</figcaption>
+				</figure>
+		        
+	        </div><!-- /.g6 -->
+
+	        <div class="g6">
+
+				<figure class="row-hero">
+					<img src="../../img/work/cochlear/screenshot-programs.png" />
+					<figcaption class="caption">
+						<h3>Programs</h3>
+						<p>Changing programs and turning on the telecoil or auto telecoil has never been easier.</p>
+					</figcaption>
+				</figure>
+		        
+	        </div><!-- /.g6 -->
+
+	        <div class="g6">
+
+				<figure class="row-hero">
+					<img src="../../img/work/cochlear/screenshot-status.png" />
+					<figcaption class="caption">
+						<h3>Status</h3>
+						<p>Easy to check your battery life or testing your microphone status anytime 24/7 worry-free.</p>
+					</figcaption>
+				</figure>
+		        
+	        </div><!-- /.g6 -->
+
+	        <div class="g6">
+
+				<figure class="row-hero">
+					<img src="../../img/work/cochlear/screenshot-settings.png" />
+					<figcaption class="caption">
+						<h3>Settings</h3>
+						<p>You can personalize your settings, such as changing the microphone capability.</p>
+					</figcaption>
+				</figure>
+		        
+	        </div><!-- /.g6 -->
+	        
+	        <div class="g12">
+				
+				<figure class="row-hero">
+					<img src="../../img/work/cochlear/screenshot-tab-buttons.png" />
+					<figcaption class="caption blurb-right">
+						<h3>Iconography</h3>
+						<p>Beautiful icons make the app a pleasure to use, and resonate the spirit of the Cochlear brand.</p>
+					</figcaption>
+				</figure>
+				
+			</div><!-- /.g12 -->
+			
+			<div class="g12">
+				
+				<figure class="row-hero">
+					<img class="righty zoom-position" src="../../img/work/cochlear/screenshot-zoom-in.png" />
+					<figcaption class="caption blurb-left">
+						<h3>Details</h3>
+						<p>User interface elements were designed with great attention to detail and focus on aesthetics that are consistent with the Cochlear brand.</p>
+					</figcaption>
+				</figure>
+				
+			</div><!-- /.g12 -->
+			
+
+		</div><!--/container-->
+		
+	</div><!-- /#final_designs -->
+	
+	<div id="whats_next">
+		
+		<div class="container">
+		
+			<div class="g12">
+				
+				<div class="intro-header inverted">
+					
+					<h2>What's Next</h2>
+					<p>The next logical step in the process would be to make this app accessible to all current Cochlear users with an iPhone. In order to do this, we to let Cochlear know that this idea is not only wanted but needed. We have already reached out to them and we are waiting to hear back from them.</p>
+					<p>If you want to show your support for this concept please share this idea.</p>
+					<a href="https://twitter.com/intent/tweet?screen_name=ArcTap&text=built%20a%20%40cochlearUS%20remote%20for%20the%20iPhone." class="twitter-mention-button" data-size="large">Tweet to @ArcTap</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				
+				</div><!-- /.intro-header -->
+				
+				<p id="disclaimer"><span>Disclaimer</span>We are not replacing the current remote but to provide a better solution.<br />This site is not operated by, sponsored by, endorsed by, or affiliated with Cochlear Ltd in any way.<br />The official Cochlear site is <a href="http://www.cochlear.com">www.cochlear.com</a>.</p>
+				
+			</div><!-- /.g12 -->
+	
+		</div><!--/container-->
+		
+	</div><!-- /#whats_next -->
+
 <?php include("../../footer.php"); ?>
