3076ea6c2c7b384fcc7a56387b50c89b97bd4577
Christoher Kennedy <kennedysgarage@gmail.com>
2012-11-24 18:39:01 -0500

    adding filter

--- js/script.js
+++ js/script.js
@@ -6,6 +6,28 @@ $(document).ready(function(){
     directionNav: false
   });
 
+  // Work Sort
+  // http://net.tutsplus.com/tutorials/javascript-ajax/creating-a-filterable-portfolio-with-jquery/
+  $('#filter a').click(function() {
+    $('ul#filter .current').removeClass('current');
+    $(this).parent().addClass('current');
+
+    var filterVal = $(this).find("span").text().toLowerCase().replace(' ','-');
+
+    if(filterVal == 'all') {
+      $('ul#portfolio li.hidden').fadeIn('slow').removeClass('hidden');
+    } else {
+      $('ul#portfolio li').each(function() {
+        if(!$(this).hasClass(filterVal)) {
+          $(this).fadeOut('normal').addClass('hidden');
+        } else {
+          $(this).fadeIn('slow').removeClass('hidden');
+        }
+      });
+    }
+    return false;
+  });
+
   // Hire Form
   // http://www.spruce.it/noise/simple-ajax-contact-form
   $("#hireForm .btn").click(function() {
--- work/work.php
+++ work/work.php
@@ -2,31 +2,38 @@
 
 <div id="work">
 
-<section id="intro">
+	<section id="intro">
+		<div class="container">
+				<h2>Our Work</h2>
+				<div id="nav">
+					<ul id="filter">
+						<li class="current"><a href=""><i class="ss-icon">&#xE9A3;</i><span>All</span></a></li>
+						<li><a href=""><i class="ss-icon">&#x1F4BB;</i><span>Web</span></a></li>
+						<li><a href=""><i class="ss-icon">&#x1F4F1;</i><span>Mobile</span></a></li>
+					</ul>
+				</div><!--/nav-->
+		</div><!--/container-->
+	</section><!--/intro-->
+
 	<div class="container">
-			<h2>Our Work</h2>
+		<div id="page">
+			<div class="content">
+
+				<ul id="portfolio">
+					<li class="g2 mobile">
+						<a href="/work/grade-spotter/"><img src="/img/work/gradespotter/gradespotter.png"/></a>
+					</li>
+					<li class="g2 web">
+						<a href=""><img src="/img/work/statuschart/statuschart.png"/></a>
+					</li>
+					<li class="g2 web">
+						<a href=""><img src="/img/work/kristinastpeter/kristinastpeter.png"/></a>
+					</li>
+				</ul>
+
+			</div><!--/content-->
+		</div><!--/page-->
 	</div><!--/container-->
-</section><!--/intro-->
-
-<div class="container">
-<div id="page">
-	<div class="content">
-
-	<div class="g2">
-		<a href="/work/grade-spotter/"><img src="/img/work/gradespotter/gradespotter.png"/></a>
-	</div><!--/g2-->
-
-	<div class="g2">
-		<a href=""><img src="/img/work/statuschart/statuschart.png"/></a>
-	</div><!--/g2-->
-
-	<div class="g2">
-		<a href=""><img src="/img/work/kristinastpeter/kristinastpeter.png"/></a>
-	</div><!--/g2-->
-
-</div><!--/content-->
-</div><!--/page-->
-</div><!--/container-->
 
 </div><!--/work-->
 
