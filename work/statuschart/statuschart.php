99a926e728b562b340b96b86ddf3dc7558379e79
Christoher Kennedy <kennedysgarage@gmail.com>
2012-11-26 18:29:32 -0500

    adding sc icon

--- work/statuschart/statuschart.php
+++ work/statuschart/statuschart.php
@@ -31,7 +31,7 @@
 	<div class="content">
 		<div class="g4">
 			<div class="title clearfix">
-				<h2><a href="https://www.statuschart.com"><img src="/img/work/statuschart/statuschart-icon.png"/> <span>Status Chart</span></a></h2>
+				<h2><a href="https://www.statuschart.com"><img src="/work/statuschart/statuschart-icon.png"/> <span>Status Chart</span></a></h2>
 				<div class="buttons"><a href="https://www.statuschart.com" class="btn btn-primary">Visit Site <i class="ss-icon">&#x1F4BB;</i></a></div>
 			</div><!--/title-->
 			<p>Status Chart is your professional dashboard. The simplest way track and share your professional milestones and all of the important things that make you who you are.</p>
