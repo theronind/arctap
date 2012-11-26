931c07949f059e274246926a149cf85c97794e71
Christoher Kennedy <kennedysgarage@gmail.com>
2012-11-26 18:22:43 -0500

    adding the base file for arcfont

---
+++ work/arcfont/arcfont.php
@@ -0,0 +1,46 @@
+<?php  $title = 'ArcFont';  include '../../header.php'; ?>
+
+<div id="work">
+
+<section id="intro">
+	<div class="container">
+			<h2>Our Work / ArcFont</h2>
+	</div><!--/container-->
+</section><!--/intro-->
+
+<div class="container">
+<div id="page">
+
+	<div class="flexslider">
+				<ul class="slides">
+				  <li>
+				  	<span><img src="/work/arcfont/arcfont.png"/></span>
+				  	<p class="flex-caption">ArcFont</p>
+				  </li>
+				  <li>
+				  	<span><img src="/work/arcfont/arcfont2.png"/></span>
+				  	<p class="flex-caption">ArcFont</p>
+				  </li>
+				  <li>
+				  	<span><img src="/work/arcfont/arcfont3.png"/></span>
+				  	<p class="flex-caption">ArcFont</p>
+				  </li>
+				</ul>
+			</div><!--/flexslider-->
+
+	<div class="content">
+		<div class="g4">
+			<div class="title clearfix">
+				<h2><a href=""><img src="/work/arcfont/arcfont-icon.png"/> <span>ArcFont</span></a></h2>
+				<div class="buttons"><a href="" class="btn btn-primary">Visit Site <i class="ss-icon">&#x1F4BB;</i></a></div>
+			</div><!--/title-->
+			<p>ArcFont is ...</p>
+		</div><!--/g4-->
+	</div><!--/content-->
+
+</div><!--/page-->
+</div><!--/container-->
+
+</div><!--/work-->
+
+<?php include("../../footer.php"); ?>
