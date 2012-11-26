8df0d050e14d19c4724c7e0ddd33215c40242ce3
Christoher Kennedy <kennedysgarage@gmail.com>
2012-11-26 09:20:48 -0500

    adding the base file for aaron

--- work/cochlear/cochlear.php
+++ work/cochlear/cochlear.php
@@ -1,188 +1,46 @@
-<?php  $title = 'Cochlear'; $css = '/work/cochlear/style.css'; include '../../header.php'; ?>
+<?php  $title = 'Cochlear';  include '../../header.php'; ?>
 
-<link rel="stylesheet" href="/css/style.css">
+<div id="work">
 
-	<div id="introduction">
-		
-		<div class="container">
-	
-			<div class="g12">
-				
-				<h1>The new Nucleus<sup>&reg;</sup> 5 Remote Assistant</h1>
-				<h2>ArcTap wants to help Cochlear continue being the leader of global hearing solutions with a simple solution.</h2>
-				
-				<img id="header-img" src="../../img/work/cochlear/header-img.png" />
-				
-			</div><!-- /.g12 -->
-	
-		</div><!--/container-->
-		
-	</div><!-- /#introduction -->
+<section id="intro">
+	<div class="container">
+			<h2>Our Work / Cochlear</h2>
+	</div><!--/container-->
+</section><!--/intro-->
 
-	<div id="the_concept">
-		
-		<div class="container">
-	
-			<div class="g12">
-				
-				<div class="intro-header">
-					
-					<h2>The Concept</h2>
-					<p>Meet <a href="http://www.aaron.im">Aaron</a>, a normal guy in his twenties, married to an awesome cochlear implant audiologist, with a kid on the way. He does most of his hobby work on his iPhone, and he also happens to be deaf. Currently Aaron is equipped with Cochlear Nucleus 5 and has to carry around a bulky remote to adjust the settings. Aaron never understood why the remote could not just be on his iPhone, so he decided to mock one up.</p>
-	
-				</div><!-- /.intro-header -->
-				
-			</div><!-- /.g12 -->
-			
-	        <div class="g5">
-				<img src="../../img/work/cochlear/aaron.jpg" alt="Aaron Hall"/>
-			</div><!-- /.g5 -->
-			<div class="g7">
-				<img src="../../img/work/cochlear/my-stuffs.jpg" alt="Aaron's stuffs"/>
-			</div><!-- /.g7 -->
-	
-		</div><!--/container-->
-		
-	</div><!-- /#the_concept -->
+<div class="container">
+<div id="page">
 
-	<div id="strategy_approach">
-		
-		<div class="container">
+	<div class="flexslider">
+				<ul class="slides">
+				  <li>
+				  	<span><img src="/work/cochlear/cochlear1.png"/></span>
+				  	<p class="flex-caption">Cochlear</p>
+				  </li>
+				  <li>
+				  	<span><img src="/work/cochlear/cochlear2.png"/></span>
+				  	<p class="flex-caption">Cochlear</p>
+				  </li>
+				  <li>
+				  	<span><img src="/work/cochlear/cochlear3.png"/></span>
+				  	<p class="flex-caption">Cochlear</p>
+				  </li>
+				</ul>
+			</div><!--/flexslider-->
 
-			<div class="g12">
-				
-				<div class="intro-header">
-					
-					<h2>Strategy &amp; Approach</h2>
-					<p>We decided that most Cochlear users already felt comfortable with the basic layout of the remote and that would be a good starting point for the design.</p>
-	
-				</div><!-- /.intro-header -->
-				
-			</div><!-- /.g12 -->
+	<div class="content">
+		<div class="g4">
+			<div class="title clearfix">
+				<h2><a href="http://cochlearapp.com"><img src="/img/work/cochlear/cochlear-icon.png"/> <span>Cochlear</span></a></h2>
+				<div class="buttons"><a href="http://cochlearapp.com" class="btn btn-primary">Visit Site <i class="ss-icon">&#x1F4BB;</i></a></div>
+			</div><!--/title-->
+			<p>Cochlear is ...</p>
+		</div><!--/g4-->
+	</div><!--/content-->
 
-		</div><!--/container-->
+</div><!--/page-->
+</div><!--/container-->
 
-		<div id="sketch">
-
-			<img src="../../img/work/cochlear/sketch.png" />
-
-		</div><!--/sketch-->
-		
-	</div><!-- /#strategy_approach -->
-
-	<div id="final_designs">
-		
-		<div class="container">
-
-			<div class="g12">
-				
-				<div class="intro-header">
-					
-					<h2>Final Designs</h2>
-	
-				</div><!-- /.intro-header -->
-				
-			</div><!-- /.g12 -->
-			
-	        <div class="g6">
-
-				<figure class="row-hero">
-					<img src="../../img/work/cochlear/screenshot-adjustments.png" />
-					<figcaption class="caption">
-						<h3>Adjustments</h3>
-						<p>Easy to change volume or sensitivity instantly at anytime such as during meetings.</p>
-					</figcaption>
-				</figure>
-		        
-	        </div><!-- /.g6 -->
-
-	        <div class="g6">
-
-				<figure class="row-hero">
-					<img src="../../img/work/cochlear/screenshot-programs.png" />
-					<figcaption class="caption">
-						<h3>Programs</h3>
-						<p>Changing programs and turning on the telecoil or auto telecoil has never been easier.</p>
-					</figcaption>
-				</figure>
-		        
-	        </div><!-- /.g6 -->
-
-	        <div class="g6">
-
-				<figure class="row-hero">
-					<img src="../../img/work/cochlear/screenshot-status.png" />
-					<figcaption class="caption">
-						<h3>Status</h3>
-						<p>Easy to check your battery life or testing your microphone status anytime 24/7 worry-free.</p>
-					</figcaption>
-				</figure>
-		        
-	        </div><!-- /.g6 -->
-
-	        <div class="g6">
-
-				<figure class="row-hero">
-					<img src="../../img/work/cochlear/screenshot-settings.png" />
-					<figcaption class="caption">
-						<h3>Settings</h3>
-						<p>You can personalize your settings, such as changing the microphone capability.</p>
-					</figcaption>
-				</figure>
-		        
-	        </div><!-- /.g6 -->
-	        
-	        <div class="g12">
-				
-				<figure class="row-hero">
-					<img src="../../img/work/cochlear/screenshot-tab-buttons.png" />
-					<figcaption class="caption blurb-right">
-						<h3>Iconography</h3>
-						<p>Beautiful icons make the app a pleasure to use, and resonate the spirit of the Cochlear brand.</p>
-					</figcaption>
-				</figure>
-				
-			</div><!-- /.g12 -->
-			
-			<div class="g12">
-				
-				<figure class="row-hero">
-					<img class="righty zoom-position" src="../../img/work/cochlear/screenshot-zoom-in.png" />
-					<figcaption class="caption blurb-left">
-						<h3>Details</h3>
-						<p>User interface elements were designed with great attention to detail and focus on aesthetics that are consistent with the Cochlear brand.</p>
-					</figcaption>
-				</figure>
-				
-			</div><!-- /.g12 -->
-			
-
-		</div><!--/container-->
-		
-	</div><!-- /#final_designs -->
-	
-	<div id="whats_next">
-		
-		<div class="container">
-		
-			<div class="g12">
-				
-				<div class="intro-header inverted">
-					
-					<h2>What's Next</h2>
-					<p>The next logical step in the process would be to make this app accessible to all current Cochlear users with an iPhone. In order to do this, we to let Cochlear know that this idea is not only wanted but needed. We have already reached out to them and we are waiting to hear back from them.</p>
-					<p>If you want to show your support for this concept please share this idea.</p>
-					<a href="https://twitter.com/intent/tweet?screen_name=ArcTap&text=built%20a%20%40cochlearUS%20remote%20for%20the%20iPhone." class="twitter-mention-button" data-size="large">Tweet to @ArcTap</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-				
-				</div><!-- /.intro-header -->
-				
-				<p id="disclaimer"><span>Disclaimer</span>We are not replacing the current remote but to provide a better solution.<br />This site is not operated by, sponsored by, endorsed by, or affiliated with Cochlear Ltd in any way.<br />The official Cochlear site is <a href="http://www.cochlear.com">www.cochlear.com</a>.</p>
-				
-			</div><!-- /.g12 -->
-	
-		</div><!--/container-->
-		
-	</div><!-- /#whats_next -->
+</div><!--/work-->
 
 <?php include("../../footer.php"); ?>
