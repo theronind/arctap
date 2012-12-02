82028c1112f82dee4c75cf80994acc3e3c8896fe
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-12-02 17:05:34 -0500

    Typography Stylesheet Media Queries Now Using Ems. HTML5 required attribute added to form elements and styled accordingly

--- hire.php
+++ hire.php
@@ -14,30 +14,31 @@
                     <div class="g4">
                         <p>You are about to make a great first step. We don't want to waste anyone's time, so we will keep this questionnaire brief. After we review this little intro of your needs we well get in touch. Thanks for your time and we look forward to speaking with you.</p>
                     </div><!--/g4-->
-                    <form id="hireForm"  method="post" role="form">
+
+                    <form id="hireForm" method="post" role="form">
                         <ul class="clearfix">
                             <li class="g2">
-                                <input type="text" id="name" name="name" placeholder="Your Name">
+                                <input type="name" id="name" name="name" placeholder="Your Name" required>
                             </li>
                             <li class="g2">
-                                <input type="email" id="email" name="email" placeholder="Your Email Address">
+                                <input type="email" id="email" name="email" placeholder="Your Email Address" required>
                             </li>
                             <li class="g2">
-                                <input type="text" id="company" name="company" placeholder="Company Name">
+                                <input type="text" id="company" name="company" placeholder="Company Name" required>
                             </li>
                             <li class="g2">
-                                <select name="budget" id="budget">
+                                <select name="budget" id="budget" required>
                                     <option selected="selected" value="DEFAULT">Estimated Budget</option>
-                                    <option value="$20,000 - $40,000">$20,000 - $40,000</option>
-                                    <option value="$40,000 - $60,000">$40,000 - $60,000</option>
-                                    <option value="$60,000 - $80,000">$60,000 - $80,000</option>
-                                    <option value="$80,000 - $140,000">$80,000 - $140,000</option>
-                                    <option value="$140,000 - $200,000">$140,000 - $200,000</option>
+                                    <option value="$20,000 &ndash; $40,000">$20,000 &ndash; $40,000</option>
+                                    <option value="$40,000 &ndash; $60,000">$40,000 &ndash; $60,000</option>
+                                    <option value="$60,000 &ndash; $80,000">$60,000 &ndash; $80,000</option>
+                                    <option value="$80,000 &ndash; $140,000">$80,000 &ndash; $140,000</option>
+                                    <option value="$140,000 &ndash; $200,000">$140,000 &ndash; $200,000</option>
                                     <option value="$200,000 Plus">$200,000 Plus</option>
                                 </select>
                             </li>
                             <li class="g4">
-                                <textarea id="message" name="message" placeholder="Give us a brief description and tell us why it will be awesome to work with you on this project."></textarea>
+                                <textarea id="message" name="message" placeholder="Give us a brief description and tell us why it will be awesome to work with you on this project." required></textarea>
                             </li>
                             <li class="g4">
                                 <input type="submit" class="btn btn-action" value="Send" name="submit">
@@ -45,7 +46,9 @@
                             <li class="success g4" style="display:none">Your message has been sent successfully.</li>
                         </ul>
                     </form>
+
                     <hr/>
+
                     <div class="g1_5">
                         <h3>Rate</h3>
                         <p class="last">$10,000/week per resource. Why charge weekly? We have found that this enables us to deliver more features, at a higher quality, over the life of a project.</p>
--- hireMail.php
+++ hireMail.php
@@ -1,34 +1,31 @@
 <?php
+if ( $_POST ) {
+    $name     = $_POST["name"];
+    $email    = $_POST["email"];
+    $company  = $_POST["company"];
+    $budget   = $_POST["budget"];
+    $message  = $_POST["message"];
 
-if($_POST){
-  $name     = $_POST["name"];
-  $email    = $_POST["email"];
-  $company  = $_POST["company"];
-  $budget   = $_POST["budget"];
-  $message  = $_POST["message"];
+    if(get_magic_quotes_gpc()) {
+        $company = stripslashes($company);
+        $message = stripslashes($message);
+    }
 
-  if(get_magic_quotes_gpc()) {
-    $company = stripslashes($company);
-    $message = stripslashes($message);
-  }
+    $address  = "team@arctap.com";
+    $subject  = "Inquiry: " . $name;
+    $body     = "<html><body>";
+    $body    .= "<strong>Name</strong>: " . $name . "<br/>";
+    $body    .= "<strong>Email</strong>: " . $email . "<br/>";
+    $body    .= "<strong>Company</strong>: " . $company . "<br/>";
+    $body    .= "<strong>Budget</strong>: " . $budget . "<br/>";
+    $body    .= "<strong>Message</strong>: " . $message;
+    $body    .= "</body></html>";
 
-  $address  = "team@arctap.com";
-  $subject  = "Inquiry: " . $name;
-  $body     = "<html><body>";
-  $body    .= "<strong>Name</strong>: " . $name . "<br/>";
-  $body    .= "<strong>Email</strong>: " . $email . "<br/>";
-  $body    .= "<strong>Company</strong>: " . $company . "<br/>";
-  $body    .= "<strong>Budget</strong>: " . $budget . "<br/>";
-  $body    .= "<strong>Message</strong>: " . $message;
-  $body    .= "</body></html>";
+    $headers  = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-  $headers  = "From: " . $email . "\r\n";
-  $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
-  $headers .= "MIME-Version: 1.0\r\n";
-  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-  //send email
-  mail($address, $subject, $body, $headers);
-}
-
-?>
+    //send email
+    mail($address, $subject, $body, $headers);
+} ?>
