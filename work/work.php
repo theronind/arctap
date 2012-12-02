9bafafa74fb60a72d0789a379347bd877f414ac5
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-12-01 19:30:23 -0500

    Aria Main Role Attribute for Content and alt attributes for img elements

--- work/work.php
+++ work/work.php
@@ -1,43 +1,43 @@
 <?php  $title = 'Work';  include '../header.php'; ?>
 
-<div id="work">
+    <div id="work">
 
-	<section id="intro">
-		<div class="container">
-				<h2>Our Work</h2>
-				<div id="nav">
-					<ul id="filter">
-						<li class="current"><a href=""><i class="ss-icon">&#xE9A3;</i><span>All</span></a></li>
-						<li><a href=""><i class="ss-icon">&#x1F4BB;</i><span>Web</span></a></li>
-						<li><a href=""><i class="ss-icon">&#x1F4F1;</i><span>Mobile</span></a></li>
-					</ul>
-				</div><!--/nav-->
-		</div><!--/container-->
-	</section><!--/intro-->
+        <section id="intro">
+            <div class="container">
+                <h2>Our Work</h2>
+                <div id="nav">
+                    <ul id="filter">
+                        <li class="current"><a href=""><i class="ss-icon">&#xE9A3;</i><span>All</span></a></li>
+                        <li><a href=""><i class="ss-icon">&#x1F4BB;</i><span>Web</span></a></li>
+                        <li><a href=""><i class="ss-icon">&#x1F4F1;</i><span>Mobile</span></a></li>
+                    </ul>
+                </div><!--/nav-->
+            </div><!--/container-->
+        </section><!--/intro-->
 
-	<div class="container">
-		<div id="page">
-			<div class="content">
+        <div class="container">
+            <div id="page">
+                <div class="content" role="main">
 
-				<ul id="portfolio">
-					<li class="g2 web">
-						<a href="/work/statuschart/"><img src="/work/statuschart/img/slide3.png"/></a>
-					</li>
-					<li class="g2 mobile">
-						<a href="/work/gradespotter/"><img src="/work/gradespotter/img/slide3.png"/></a>
-					</li>
-					<li class="g2 web">
-						<a href="/work/kristinastpeter/"><img src="/work/kristinastpeter/img/slide3.png"/></a>
-					</li>
-					<li class="g2 web">
-						<a href="/work/theeastwing/"><img src="/work/theeastwing/img/thumb.png"/></a>
-					</li>
-				</ul>
+                    <ul id="portfolio">
+                        <li class="g2 web">
+                            <a href="/work/statuschart/" rel="bookmark"><img src="/work/statuschart/img/slide3.png" alt=""></a>
+                        </li>
+                        <li class="g2 mobile">
+                            <a href="/work/gradespotter/" rel="bookmark"><img src="/work/gradespotter/img/slide3.png" alt=""></a>
+                        </li>
+                        <li class="g2 web">
+                            <a href="/work/kristinastpeter/" rel="bookmark"><img src="/work/kristinastpeter/img/slide3.png" alt=""></a>
+                        </li>
+                        <li class="g2 web">
+                            <a href="/work/theeastwing/" rel="bookmark"><img src="/work/theeastwing/img/thumb.png" alt=""></a>
+                        </li>
+                    </ul>
 
-			</div><!--/content-->
-		</div><!--/page-->
-	</div><!--/container-->
+                </div><!--/content-->
+            </div><!--/page-->
+        </div><!--/container-->
 
-</div><!--/work-->
+    </div><!--/work-->
 
 <?php include("../footer.php"); ?>
