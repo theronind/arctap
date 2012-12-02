10d70c629d93ea6c4317268a2765eca29335e0d1
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-12-01 19:49:47 -0500

    East Wing Image Optimization, Main Role for Content, Indentation Adjustments for Coding Consistency

--- work/theeastwing/theeastwing.php
+++ work/theeastwing/theeastwing.php
@@ -1,42 +1,44 @@
 <?php  $title = 'The East Wing';  include '../../header.php'; ?>
 
-<div id="work">
+    <div id="work">
 
-<section id="intro">
-	<div class="container">
-			<h2>Our Work / The East Wing</h2>
-	</div><!--/container-->
-</section><!--/intro-->
+        <section id="intro">
+            <div class="container">
+                    <h2>Our Work / The East Wing</h2>
+            </div><!--/container-->
+        </section><!--/intro-->
 
-<div class="container">
-<div id="page">
+        <div class="container">
+            <div id="page">
 
-	<div class="flexslider">
-				<ul class="slides">
-				  <li>
-				  	<span><img src="/work/theeastwing/img/slide1.png"/></span>
-				  	<p class="flex-caption">The East Wing</p>
-				  </li>
-				  <li>
-				  	<span><img src="/work/theeastwing/img/slide2.png"/></span>
-				  	<p class="flex-caption">The East Wing</p>
-				  </li>
-				</ul>
-			</div><!--/flexslider-->
+                <div class="flexslider">
+                    <ul class="slides">
+                        <li>
+                            <span><img src="/work/theeastwing/img/slide1.png" alt=""></span>
+                            <p class="flex-caption">The East Wing</p>
+                        </li>
+                        <li>
+                            <span><img src="/work/theeastwing/img/slide2.png" alt=""></span>
+                            <p class="flex-caption">The East Wing</p>
+                        </li>
+                    </ul>
+                </div><!--/flexslider-->
 
-	<div class="content">
-		<div class="g4">
-			<div class="title clearfix">
-				<h2><a href="http://theeastwing.net/"><img src="/work/theeastwing/img/icon.png"/> <span>The East Wing</span></a></h2>
-				<div class="buttons"><a href="http://theeastwing.net/" class="btn btn-primary">Visit Site <i class="ss-icon">&#x1F4BB;</i></a></div>
-			</div><!--/title-->
-			<p>The East Wing is a weekly show that talks about design, user experience, problem solving and the keys to creating producst with value.</p>
-		</div><!--/g4-->
-	</div><!--/content-->
+                <div class="content" role="main">
+                    <div class="g4">
+                        <div class="title clearfix">
+                            <h2><a href="http://theeastwing.net/" rel="external"><img src="/work/theeastwing/img/icon.png" alt=""><span>The East Wing</span></a></h2>
+                            <div class="buttons">
+                                <a href="http://theeastwing.net/" class="btn btn-primary" rel="external">Visit Site<i class="ss-icon">&#x1F4BB;</i></a>
+                            </div>
+                        </div><!--/title-->
+                        <p>The East Wing is a weekly show that talks about design, user experience, problem solving and the keys to creating producst with value.</p>
+                    </div><!--/g4-->
+                </div><!--/content-->
 
-</div><!--/page-->
-</div><!--/container-->
+            </div><!--/page-->
+        </div><!--/container-->
 
-</div><!--/work-->
+    </div><!--/work-->
 
 <?php include("../../footer.php"); ?>
