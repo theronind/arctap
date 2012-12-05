668ac7be60e0f80144f3513d6ffe8b7c0e9189ec
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-12-05 13:10:59 -0500

    background color removal for nyc photo on about page

--- config.rb
+++ config.rb
@@ -36,7 +36,7 @@ output_style = :compressed
 environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = false
+relative_assets = true
 
 line_comments = false
 color_output = false
