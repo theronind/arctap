33cce0acebd4e0c6e7f4a0344f2a1ddce64e4d67
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-12-15 18:05:07 -0500

    removed html from mail confirmation

--- hireMail.php
+++ hireMail.php
@@ -27,21 +27,19 @@ if ( $_POST ) {
 
     // admin email
     $address  = "grayghost@grayghostvisuals.com";
-    $subject  = "Inquiry: " . $name;
-    $body     = "<html><body>";
-    $body    .= "<strong>Name</strong>: " . $name . "<br/>";
-    $body    .= "<strong>Company</strong>: " . $company . "<br/>";
-    $body    .= "<strong>Phone</strong>: " . $phone . "<br/>";
-    $body    .= "<strong>Email</strong>: " . $email . "<br/>";
-    $body    .= "<strong>Website</strong>: " . $website . "<br/>";
-    $body    .= "<strong>Budget</strong>: " . $budget . "<br/>";
-    $body    .= "<strong>Message</strong>: " . $message;
-    $body    .= "</body></html>";
+    $subject  = "Inquiry: " . $name. '\n';
+    $body    .= "Name: " . $name . '\n';
+    $body    .= "Company: " . $company . '\n';
+    $body    .= "Phone: " . $phone . '\n';
+    $body    .= "Email: " . $email . '\n';
+    $body    .= "Website: " . $website . '\n';
+    $body    .= "Budget: " . $budget . '\n';
+    $body    .= "Message: " . $message;
 
-    $headers  = "From: " . $email . "\r\n";
-    $headers .= "Reply-To: ". strip_tags($address) . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $headers  = "From: " . $email . '\r\n';
+    $headers .= "Reply-To: ". strip_tags($address) . '\r\n';
+    $headers .= 'MIME-Version: 1.0\r\n';
+    $headers .= 'Content-Type: text/html; charset=ISO-8859-1\r\n';
 
     if ( isset( $_POST['submit'] ) || $error == true ) {
 
