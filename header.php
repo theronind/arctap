82352c45bbdd4dabcc99e357d33db0d0c35b7483
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-03-14 10:27:21 -0400

    Partials and Cleaning

--- header.php
+++ header.php
@@ -1,22 +1,22 @@
 <!doctype html>
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
 <!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
-<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js gt-ie9" lang="en"><!--<![endif]-->
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>ArcTap - A Freelance Collective</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-    <link rel="shortcut icon" href="/favicon.png">
-    <link rel="dns-prefetch" href="//fonts.googleapis.com">
-    <link rel="dns-prefetch" href="//www.google-analytics.com">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/style.css">
+	<title>ArcTap - A Freelance Collective</title>
 
-    <script src="/js/libs/modernizr-2.6.2.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+	<link rel="shortcut icon" href="/favicon.png">
+	<link rel="dns-prefetch" href="//fonts.googleapis.com">
+	<link rel="dns-prefetch" href="//www.google-analytics.com">
+
+	<link rel="stylesheet" href="/css/style.css">
+
+	<script src="/js/libs/modernizr-2.6.2.min.js"></script>
 </head>
 <body>
-<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
--- index.php
+++ index.php
@@ -1,38 +1,47 @@
 <?php include 'header.php'; ?>
 
-  <header>
+	<header role="banner">
 
-    <img src="/img/ginger.svg"/>
+		<img src="/img/ginger.svg" alt="">
 
-    <div class="welcome">
-      <h1>ArcTap</h1>
-      <p>We're a community of amazing web professionals with the common goal of working together with other like-minded freelancers. Our passion is to help clients build amazing experiences for their customers.</p>
-    </div><!--/welcome-->
+		<div class="welcome">
+			<h1>ArcTap</h1>
 
-  </header>
+			<p>We're a community of amazing web professionals with the common goal of working together with other like-minded freelancers. Our passion is to help clients build amazing experiences for their customers.</p>
+		</div>
+		<!--/welcome-->
 
-  <ul class="members clearfix">
-    <li><img src="/img/members/kennedy.jpg"/></li>
-    <li><img src="/img/members/dennis-gaebel.png"/></li>
-    <li><img src="/img/members/tim-smith.png"/></li>
-    <li><img src="/img/members/nicole-dominguez.jpg"/></li>
-    <li><img src="/img/members/frank-denbow.jpg"/></li>
-    <li><img src="/img/members/brian-revak.jpg"/></li>
-    <li><img src="/img/members/aaron-hall.jpg"/></li>
-    <li><img src="/img/members/chris-jeane.jpg"/></li>
-    <li><img src="/img/members/jarod-reyes.jpg"/></li>
-    <li><img src="/img/members/john-britton.jpg"/></li>
-    <li><img src="/img/members/rodrigo-sanchez.jpg"/></li>
-    <li><img src="/img/members/jon-gottfried.jpg"/></li>
-    <li><img src="/img/members/kevin-quinn.jpg"/></li>
+	</header>
 
-    <li><p>Interested in hiring one of us?<br/>Feel free to shoot us an email<br/><a href="mailto:hello@arctap.com">Hello@ArcTap.com</a></p></li>
-  </ul>
+	<section role="main">
 
-  <footer>
-    <img src="/img/logo.svg"/>
-    <h2>ArcTap</h2>
-    <p>a freelance collective</p>
-  </footer>
+		<ul class="members clearfix">
+			<li><img src="/img/members/kennedy.jpg" alt=""></li>
+			<li><img src="/img/members/dennis-gaebel.png" alt=""></li>
+			<li><img src="/img/members/tim-smith.png" alt=""></li>
+			<li><img src="/img/members/nicole-dominguez.jpg" alt=""></li>
+			<li><img src="/img/members/frank-denbow.jpg" alt=""></li>
+			<li><img src="/img/members/brian-revak.jpg" alt=""></li>
+			<li><img src="/img/members/aaron-hall.jpg" alt=""></li>
+			<li><img src="/img/members/chris-jeane.jpg" alt=""></li>
+			<li><img src="/img/members/jarod-reyes.jpg" alt=""></li>
+			<li><img src="/img/members/john-britton.jpg" alt=""></li>
+			<li><img src="/img/members/rodrigo-sanchez.jpg" alt=""></li>
+			<li><img src="/img/members/jon-gottfried.jpg" alt=""></li>
+			<li><img src="/img/members/kevin-quinn.jpg" alt=""></li>
+			<li><p>Interested in hiring one of us?<br/>Feel free to shoot us an email<br/><a href="mailto:hello@arctap.com">Hello@ArcTap.com</a></p></li>
+		</ul>
+
+	</section>
+
+	<footer role="contentinfo">
+
+		<img src="/img/logo.svg" alt="">
+
+		<h2>ArcTap</h2>
+
+		<p>a freelance collective</p>
+
+	</footer>
 
 <?php include("footer.php"); ?>
