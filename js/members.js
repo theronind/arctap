c0b1839a2c73d62a5ec50bace8ac90149f2184aa
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-03-16 12:27:51 -0400

    Removes unwanted social links markup. Adds data attribute for twitter.

--- js/members.js
+++ js/members.js
@@ -1,26 +1,27 @@
-// social links
-$('.members').find('[data-toggle]').on('click', function(event) {
+function modalsContent(event) {
 
 	var item = event.currentTarget,
 		memberPic = item.children[0].src,
-        twitterHref = $('.social li a[href="https://twitter.com/"]').attr('href'),
-        instagramHref = $('.social li a[href="http://instagram.com/"]').attr('href');
+		twitterHref = $('.social li a[href="https://twitter.com/"]').attr('href');
 
-    // set the image
+	// set the image
 	$('.face img').attr('src', memberPic);
 
-    // set the name
+	// set the name
 	$('.member-name').text($(this).data('name'));
 
-    // set the role
+	// set the role
 	$('.member-title').text($(this).data('role'));
 
-    // add the bio
+	// add the bio
 	$('.member-bio').text($(this).data('bio'));
 
-    // add the tags
+	// add the tags
 	$('.tags').text($(this).data('tags'));
 
-    $('.social li a[href="https://twitter.com/"]').attr('href', twitterHref + $(this).data('twitter'));
-    $('.social li a[href="http://instagram.com/"]').attr('href', instagramHref + $(this).data('instagram'));
-});
+	$('.social li a[href="https://twitter.com/"]').attr('href', twitterHref + $(this).data('twitter'));
+
+}
+
+
+$('.members').find('[data-toggle]').on('click', modalsContent);
