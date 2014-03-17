88fcccfbba9d207bd29740557eb9b0c383ea214b
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-03-16 20:51:19 -0400

    modal cleanup

--- js/members.js
+++ js/members.js
@@ -2,7 +2,6 @@ function modalsContent(event) {
 
 	var item = event.currentTarget,
 		memberPic = item.children[0].src;
-		console.log(memberPic);
 
 	// set the image
 	$('.face img').attr('src', memberPic);
@@ -17,7 +16,7 @@ function modalsContent(event) {
 	$('.member-bio').append().html($(this).data('bio'));
 
 	// add the tags
-	$('.tags').text($(this).data('tags'));
+	$('.member-tags').text($(this).data('tags'));
 
 	// Add name as a hash to URL
 	window.location.hash = $('.member-name').text().replace(' ', '');
--- modal.php
+++ modal.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="arcTapModal" tabindex="-1" aria-labelledby="arcMember" aria-hidden="true" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
 
@@ -9,14 +9,17 @@
 
             <div class="bio">
                 <div class="modal-header">
-                    <div class="close" data-dismiss="modal" aria-hidden="true"><i class="ss-icon ss-standard">&#x2421;</i></div>
-                    <h3 class="modal-title member-name" id="myModalLabel"></h3>
+                    <div class="close" data-dismiss="modal" aria-hidden="true">
+                        <i class="ss-icon ss-standard">&#x2421;</i>
+                    </div>
+
+                    <h3 class="modal-title member-name" id="arcMember"></h3>
                 </div>
 
                 <div class="modal-body">
                     <h4 class="member-title"></h4>
                     <p class="member-bio"></p>
-                    <p class="tags"></p>
+                    <p class="member-tags"></p>
                 </div>
             </div>
 
