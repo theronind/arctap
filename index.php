e5d6aed12035d1d0dcf8d8c827835d8193956191
Kennedy <kennedysgarage@gmail.com>
2014-03-18 00:53:49 -0400

    updating some basic info

--- index.php
+++ index.php
@@ -11,19 +11,19 @@
 
 	<section role="main">
 		<ul class="members clearfix">
-			<li data-toggle="modal" data-target="#arcTapModal" data-name="Kennedy" data-role="Designer" data-tags="#ux, #design, #mobile, #html, #css" data-bio="A naively-optimistic entrepreneur, professional hacker, UX strategist, &amp; mobile UI designer with a passion for startups. He is currently working at Google as an Interaction Designer. follow me on &lt;a href=&quot; http://twitter.com/kennedysgarage&quot;&gt;twitter&lt;/a&gt;"><img src="/img/members/kennedy.jpg" alt=""></li>
+			<li data-toggle="modal" data-target="#arcTapModal" data-name="Kennedy" data-role="Designer" data-tags="#mobile, #ios, #android, #ux, #design, #html, #css" data-bio="A naively-optimistic entrepreneur, professional hacker, UX strategist, &amp; mobile UI designer with a passion for startups. He is currently working at Google as an Interaction Designer. Follow him on Twitter &lt;a href=&quot; http://twitter.com/kennedysgarage&quot;&gt;@KennedysGarage&lt;/a&gt;."><img src="/img/members/kennedy.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Dennis Gaebel" data-role="Developer" data-tags="#ux, #design, #rwd, #html, #css #js" data-bio="this is my bio. follow me on &lt;a href=&quot; http://twitter.com/gryghostvisuals&quot;&gt;twitter&lt;/a&gt;"><img src="/img/members/dennis-gaebel.png" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Tim Smith" data-role="Designer" data-tags="" data-bio=""><img src="/img/members/tim-smith.png" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Nicole Dominguez" data-role="Designer" data-tags="" data-bio=""><img src="/img/members/nicole-dominguez.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Frank Denbow" data-role="Developer" data-tags="" data-bio=""><img src="/img/members/frank-denbow.jpg" alt=""></li>
-			<li data-toggle="modal" data-target="#arcTapModal" data-name="Brian Revak" data-role="Agent" data-tags="" data-bio=""><img src="/img/members/brian-revak.jpg" alt=""></li>
+			<li data-toggle="modal" data-target="#arcTapModal" data-name="Brian Revak" data-role="Business Dev" data-tags="" data-bio=""><img src="/img/members/brian-revak.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Aaron Hall" data-role="Designer" data-tags="" data-bio=""><img src="/img/members/aaron-hall.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Chris Jeane" data-role="Developer" data-tags="" data-bio=""><img src="/img/members/chris-jeane.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Jarod Reyes" data-role="Developer" data-tags="" data-bio=""><img src="/img/members/jarod-reyes.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="John Britton" data-role="Developer" data-tags="" data-bio=""><img src="/img/members/john-britton.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Rodrigo Sanchez" data-role="Designer" data-tags="" data-bio=""><img src="/img/members/rodrigo-sanchez.jpg" alt=""></li>
 			<li data-toggle="modal" data-target="#arcTapModal" data-name="Jon Gottfried" data-role="Developer" data-tags="" data-bio=""><img src="/img/members/jon-gottfried.jpg" alt=""></li>
-			<li data-toggle="modal" data-target="#arcTapModal" data-name="Kevin Quinn" data-role="Audio" data-tags="" data-bio=""><img src="/img/members/kevin-quinn.jpg" alt=""></li>
+			<li data-toggle="modal" data-target="#arcTapModal" data-name="Kevin Quinn" data-role="Voice Actor" data-tags="" data-bio=""><img src="/img/members/kevin-quinn.jpg" alt=""></li>
 			<li><p>Interested in hiring one of us?<br/>Feel free to shoot us an email<br/><a href="mailto:hello@arctap.com">Hello@ArcTap.com</a></p></li>
 		</ul>
 	</section>
